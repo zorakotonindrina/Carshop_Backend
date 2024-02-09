5a786b74af11999da29d6e265e9ba9bcd908cd52
zorakotonindrina <120090215+zorakotonindrina@users.noreply.github.com>
2024-02-09 20:18:07 +0100

    angular config

--- src/main/java/com/example/Carshop/config/CorsConfig.java
+++ src/main/java/com/example/Carshop/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000") // Ajoutez le domaine de votre application React
+            .allowedOrigins("http://localhost:3000", "http://localhost:8100") // Ajoutez le domaine de votre application React
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowCredentials(true);
     }
